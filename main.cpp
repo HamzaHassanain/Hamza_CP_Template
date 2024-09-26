bb137261d96048bcfe7ec75efb8caf24e3a36168
hamza hassanain <hamzahassanain067@gmail.com>
2024-09-26 16:08:11 +0300

    hamza added changes:

--- main.cpp
+++ main.cpp
@@ -1,15 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+const string dl = "\n";
+typedef long long ll;
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define rv(exp) return void(cout << exp << dl)
+template <typename T>
+ostream &operator<<(ostream &out, vector<T> &vec)
+{
+    for (auto &x : vec)
+        out << x << " ";
+    return out;
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T> &vec)
+{
+    for (auto &x : vec)
+        in >> x;
+    return in;
+}
+
+void solve()
+{
+    
+}
 
 int main()
 {
-    int W;
-    cin >> W;
 
-    if (W == 0)
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int tc = 1;
+
+    cin >> tc;
+
+    for (int t = 1; t <= tc; t++)
     {
-        cout << "NO" << endl;
+        solve();
     }
-
-    cout << W << endl;
+    return 0;
 }
